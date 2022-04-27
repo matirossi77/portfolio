eb06721e2a97043fd677df670fd94dd43368ccf2
matirossi77 <matirossi77@gmail.com>
2022-04-27 08:57:40 -0300

    cambios

---
+++ header.php
@@ -0,0 +1,41 @@
+<header>
+      <nav class="navbar navbar-expand-md mb-4">
+        <div class="container">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarCollapse"
+            aria-controls="navbarCollapse"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <i class="fas fa-bars"></i>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav me-auto mb-2 mb-md-0 mt-2">
+              <li class="nav-item active">
+                <a class="nav-link px-4 py-1" href="index.html">Inicio</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link px-4 py-1" href="sobre-mi.html">Sobre mí</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link px-4 py-1" href="proyectos.html"
+                  >Proyectos</a
+                >
+              </li>
+              <li class="nav-item" aria-current="page">
+                <a class="nav-link px-4 py-1" href="contacto.html">Contacto</a>
+              </li>
+            </ul>
+            <div class="d-flex">
+              <a href="" class="btn btn-outline-success btn-rojo" type="submit">
+                Descargar mi CV
+                <i class="fa-solid fa-download"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
